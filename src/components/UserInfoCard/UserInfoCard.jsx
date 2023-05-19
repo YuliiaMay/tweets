e5294b6bf23577708afa3f7694d9c8bc013bc5a7
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-05-19 13:47:26 +0300

    FIX: card

--- src/components/UserInfoCard/UserInfoCard.jsx
+++ src/components/UserInfoCard/UserInfoCard.jsx
@@ -1,31 +1,34 @@
-import { Card } from "./UserInfoCard.styled";
+import { Card, Wrapper, CardLogo, CardImg, StyledLine, Avatar, AvatarBorder, InfoBlock } from "./UserInfoCard.styled";
 import goitLogo from "../../images/card/logo.svg";
 import cardBackground from "../../images/card/background.png"
 
 
+const ellipsePath = new URL('../../images/card/ellipse.svg', import.meta.url);
+
 const UserInfoCard = ({ userName, tweetsCount, followersCount, avatar }) => {
     return (
         <Card>
-            <div>
-                <img src={goitLogo} alt="GOIT icon" />
-                <img src={cardBackground} alt="backgroung" />
-            </div>
+            <Wrapper>
+                <CardLogo src={goitLogo} alt="logo"/>
+                <CardImg src={cardBackground} alt="backgroung" />
+                <StyledLine />
+                <Avatar src={avatar} alt="avatar" />
+                <AvatarBorder src={ellipsePath} alt="ellipse" />
 
-            <img src="" alt="avatar" />
 
-            <div>
-                <p>{userName}</p>
-                <p>
-                    <span>{tweetsCount}</span>
-                    tweets
-                </p>
-                <p>
-                    <span>{followersCount}</span>
-                    followers
-                </p>
-                <button>follow</button>
-            </div>
-            
+                <InfoBlock>
+                    <p>{userName}</p>
+                    <p>
+                        <span>{tweetsCount}</span>
+                        tweets
+                    </p>
+                    <p>
+                        <span>{followersCount}</span>
+                        followers
+                    </p>
+                    <button>follow</button>
+                </InfoBlock>
+            </Wrapper>
         </Card>
     );
 }
--- src/components/UserInfoCard/UserInfoCard.styled.js
+++ src/components/UserInfoCard/UserInfoCard.styled.js
@@ -7,3 +7,59 @@ export const Card = styled.li`
 
     background: linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%);
 `;
+
+export const Wrapper = styled.div`
+    position: relative;
+    top: 0;
+    left: 0;
+`;
+
+export const CardLogo = styled.img`
+    width: 76px;
+    height: 22px;
+    position: relative;
+    top: 20px;
+    left: 20px;
+`;
+
+export const CardImg = styled.img`
+    width: 308px;
+    height: 168px;
+    position: relative;
+    left: 36px;
+`;
+
+export const StyledLine = styled.div`
+    width: 380px;
+    height: 8px;
+    position: relative;
+    top: 18px;
+    background: #EBD8FF;
+    box-shadow: 0px 3.4369285106658936px 2.5776965618133545px 0px #FBF8FF inset;
+    box-shadow: 0px 3.4369285106658936px 3.4369285106658936px 0px #0000000F;
+    box-shadow: 0px -1.7184642553329468px 3.4369285106658936px 0px #AE7BE3 inset;
+`;
+
+export const Avatar = styled.img`
+    position: absolute;
+    width: 62px;
+    height: 62px;
+    border-radius: 85.92px;
+    top: 178px;
+    left: 150px;
+`;
+
+export const AvatarBorder = styled.img`
+    position: absolute;
+    // width: 80px;
+    // height: 80px;
+    top: 168px;
+    left: 136px;
+`;
+
+export const InfoBlock = styled.div`
+    position: relative;
+    top: 62px;
+    display: 
+`;
+
